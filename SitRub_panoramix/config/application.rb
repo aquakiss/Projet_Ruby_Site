546619118113553b3f305fe9e3c83be3c7a939b5
Aquakiss <romain.menard17@gmail.com>
2015-05-11 14:45:57 +0200

    bootstrap-sass installé fonctionnel

--- SitRub_panoramix/config/application.rb
+++ SitRub_panoramix/config/application.rb
@@ -21,17 +21,18 @@ module SitRubPanoramix
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+# Bower asset paths
+  Rails.root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
+    config.sass.load_paths << bower_path
+    config.assets.paths << bower_path
+  end
+
+  # Precompile Bootstrap fonts
+  config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
+  # Minimum Sass number precision required by bootstrap-sass
+  ::Sass::Script::Value::Number.precision = [8, ::Sass::Script::Value::Number.precision].max
+    
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
 
-# Bower asset paths
-root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
-  config.sass.load_paths << bower_path
-  config.assets.paths << bower_path
-end
-
-# Precompile Bootstrap fonts
-config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
-# Minimum Sass number precision required by bootstrap-sass
-::Sass::Script::Value::Number.precision = [8, ::Sass::Script::Value::Number.precision].max

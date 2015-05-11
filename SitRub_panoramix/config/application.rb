5fc06027f52ac4f542f34d84412ab264783d70a2
Aquakiss <romain.menard17@gmail.com>
2015-05-11 14:12:46 +0200

    test add boostrap

--- SitRub_panoramix/config/application.rb
+++ SitRub_panoramix/config/application.rb
@@ -24,3 +24,14 @@ module SitRubPanoramix
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
+
+# Bower asset paths
+root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
+  config.sass.load_paths << bower_path
+  config.assets.paths << bower_path
+end
+
+# Precompile Bootstrap fonts
+config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
+# Minimum Sass number precision required by bootstrap-sass
+::Sass::Script::Value::Number.precision = [8, ::Sass::Script::Value::Number.precision].max

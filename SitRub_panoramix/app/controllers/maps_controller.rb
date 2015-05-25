a43df1dc0208b904b74de259c151814b5626b0d7
Aquakiss <romain.menard17@gmail.com>
2015-05-25 16:13:15 +0200

    affiche all point link at a map + start create point in map

--- SitRub_panoramix/app/controllers/maps_controller.rb
+++ SitRub_panoramix/app/controllers/maps_controller.rb
@@ -11,7 +11,24 @@ class MapsController < ApplicationController
   # GET /maps/1
   # GET /maps/1.json
   def show
-    @point = Point.new
+  end
+
+  def createPoint
+    @point = Point.new(point_params)
+    respond_to do |format|
+      if @point.save
+        format.html { redirect_to @point, notice: 'Point was successfully created.' }
+        format.json { render :show, status: :created, location: @point }
+      else
+        format.html { render :new }
+        format.json { render json: @point.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def allpoint map_di
+    @nb_map = Point.where("map_id = #{map_di}").count
+    return @nb_map
   end
 
   # GET /maps/new
@@ -40,16 +57,11 @@ class MapsController < ApplicationController
     end
   end
 
-  #nested point
-  private
-  def map_params
-    params.require(:map).permit(:name, points_attributes: [:id, :x, :y, :photo_url])
-  end
-
   # PATCH/PUT /maps/1
   # PATCH/PUT /maps/1.json
   def update
     respond_to do |format|
+      @map = Map.find(params[:id])
       if @map.update(map_params)
         format.html { redirect_to @map, notice: 'Map was successfully updated.' }
         format.json { render :show, status: :ok, location: @map }
--- SitRub_panoramix/config/routes.rb
+++ SitRub_panoramix/config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'main#index'
+  root 'maps#index'
+
+  post "/maps/:id" => "maps#show"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
